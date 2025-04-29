e73e0efa65188c002f66df43ea3329c415a35167
gismo2206 <132850666+gismo2206@users.noreply.github.com>
2025-04-29 22:08:41 +0200

    Update connessione.php

--- connessione.php
+++ connessione.php
@@ -1,45 +1,41 @@
 <?php
-// fase 1: connessione e riconoscimento
 $db_host = "localhost";
 $db_user = "root";
 $db_password = "";
-$db ="articolitelefonici";
+$db = "articolitelefonici";
 
 $db_connection = new mysqli($db_host, $db_user, $db_password, $db);
 
-if ($db_connection->connect_errno) 
- {
-    echo("Si è verificato il seguente problema tecnico: " .           $db_connection->connect_error);
+if ($db_connection->connect_errno) {
+    echo "Problema tecnico: " . $db_connection->connect_error;
     exit;
- } 
- echo "Connessione Stabilita correttamente!";
+}
 
- // 2 fase : scrittura e lancio della query 
+echo "Connessione stabilita correttamente!<br>";
 
- $tabname="telefoni";
+$tabname = "telefoni";
 
- $creatab="create table $tabname (idtel int auto_increment primary key,
- marca varchar(40), modello varchar(40), prezzo decimal(10,2))";
-  
- $result=$db_connection->query($creatab);
- 
- if (!$result)
-   echo "errore nella query!";
- else
-   echo "Tabella creata correttamente!";
-  
-  // inserisco i dati nella tabella
+$creatab = "CREATE TABLE IF NOT EXISTS $tabname (
+    idtel INT AUTO_INCREMENT PRIMARY KEY,
+    marca VARCHAR(40),
+    modello VARCHAR(40),
+    prezzo DECIMAL(10,2)
+)";
 
-  $insert="insert into $tabname (marca,modello,prezzo) values ('Nokia','3310',30.00), ('Motorola','H50',380.00)";
+if (!$db_connection->query($creatab)) {
+    echo "Errore nella creazione della tabella!<br>";
+} else {
+    echo "Tabella creata correttamente (o già esistente).<br>";
+}
 
- $result=$db_connection->query($insert);
- if (!$result)
-   echo "errore nella query!";
- else
-   echo "dati inseriti nella Tabella correttamente!";
-  
+$insert = "INSERT INTO $tabname (marca, modello, prezzo)
+VALUES ('Nokia','3310',30.00), ('Motorola','H50',380.00)";
 
- 
-          
+if (!$db_connection->query($insert)) {
+    echo "Errore durante l'inserimento dei dati (probabilmente già inseriti).<br>";
+} else {
+    echo "Dati inseriti correttamente nella tabella.<br>";
+}
 
+$db_connection->close();
 ?>
