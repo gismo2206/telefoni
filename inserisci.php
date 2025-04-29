097b909b7b06328860acbfcd24b249cabe4fd444
gismo2206 <132850666+gismo2206@users.noreply.github.com>
2025-04-29 22:09:37 +0200

    Update inserisci.php

--- inserisci.php
+++ inserisci.php
@@ -1,13 +1,32 @@
 <?php
+$db_host = "localhost";
+$db_user = "root";
+$db_password = "";
+$db = "articolitelefonici";
+
+$db_connection = new mysqli($db_host, $db_user, $db_password, $db);
+
+if ($db_connection->connect_errno) {
+    echo "Errore di connessione: " . $db_connection->connect_error;
+    exit;
+}
+
 $marca = $_POST['marca'];
 $modello = $_POST['modello'];
 $prezzo = $_POST['prezzo'];
 
+$insert = "INSERT INTO telefoni (marca, modello, prezzo)
+           VALUES (?, ?, ?)";
 
-$insert="insert into telefoni ";
-$result=$db_connection->query($insert);
-if (!$result)
-    echo "errore nella query!";
-else
-    echo "dati inseriti nella Tabella correttamente!";
+$stmt = $db_connection->prepare($insert);
+$stmt->bind_param("ssd", $marca, $modello, $prezzo);
+
+if ($stmt->execute()) {
+    echo "Telefono inserito correttamente!";
+} else {
+    echo "Errore durante l'inserimento: " . $stmt->error;
+}
+
+$stmt->close();
+$db_connection->close();
 ?>
