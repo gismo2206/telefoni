d7c07bf1f1aa46c2077c94610087d483e41a825b
gismo2206 <132850666+gismo2206@users.noreply.github.com>
2025-04-29 22:10:04 +0200

    Update visualizza.php

--- visualizza.php
+++ visualizza.php
@@ -1,48 +1,28 @@
 <?php
-
-// fase 1: connessione e riconoscimento
-
 $db_host = "localhost";
 $db_user = "root";
 $db_password = "";
-$db ="articolitelefonici";
+$db = "articolitelefonici";
 
 $db_connection = new mysqli($db_host, $db_user, $db_password, $db);
 
-if ($db_connection->connect_errno) 
- {
-    echo("Si è verificato il seguente problema tecnico: " .           $db_connection->connect_error);
+if ($db_connection->connect_errno) {
+    echo "Errore: " . $db_connection->connect_error;
     exit;
- } 
- echo "Connessione Stabilita correttamente!<br>";
-
- // 2 fase : scrittura e lancio della query 
-
- $tabname="telefoni";
-
- $select="select * from $tabname";
-  
- $result=$db_connection->query($select);
- 
- $rows=$result->num_rows;
-
- echo "Sono presenti $rows records<br />";
-
- echo "<center> <table border=1 bordercolor=green>
-      <tr><th>ID Telefono</th> <th>Marca</th> <th>Modello</th> <th>Prezzo</th></tr>";
-
-
- if($rows>0)
-  {
-   while($rows = $result->fetch_assoc())
-     {
-	echo "<tr><td>".$rows['idtel']. "</td><td>". $rows['marca']."</td><td>". $rows['modello']. "</td><td>". $rows['prezzo']."</td></tr>";
-   }
 }
 
-echo "</table></center>";
+$select = "SELECT * FROM telefoni";
+$result = $db_connection->query($select);
 
-$result->free();
+if ($result->num_rows > 0) {
+    echo "<h2>Elenco Telefoni:</h2><ul>";
+    while ($row = $result->fetch_assoc()) {
+        echo "<li>" . $row["marca"] . " " . $row["modello"] . " - €" . $row["prezzo"] . "</li>";
+    }
+    echo "</ul>";
+} else {
+    echo "Nessun telefono trovato.";
+}
+
 $db_connection->close();
-
 ?>
